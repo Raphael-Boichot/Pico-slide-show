a5742d6ea45c2a7bae98cbd1a582a5684bd3a2da
Raphaël BOICHOT <raphael.boichot@gmail.com>
2025-06-22 18:08:58 +0200

    Add files via upload

--- Pico_slide_show/Make_header_from_saves.m
+++ Pico_slide_show/Make_header_from_saves.m
@@ -10,6 +10,7 @@ chunk_size = 3584;          % 3584 bytes per image
 chunks_per_file = 30;       % 30 images per .sav file
 start_offset = 8193;        % Starting byte for first chunk
 block_spacing = 4096;       % Bytes between chunks
+maximum_file_number = 540;  % limits data to about 2MB
 
 % Collect save files %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 listing = dir('./Saves/*.sav*');
@@ -25,25 +26,25 @@ disp('Extracting image data from saves');
 idx = 1;
 
 for i = 1:num_files
-    filename = ['./saves/', listing(i).name];
-    fid = fopen(filename, 'r');
-    if fid == -1
-        warning('Could not open file: %s', filename);
-        continue;
-    end
-    binary = fread(fid, inf, 'uint8');
-    fclose(fid);
+  filename = ['./saves/', listing(i).name];
+  fid = fopen(filename, 'r');
+  if fid == -1
+    warning('Could not open file: %s', filename);
+    continue;
+  end
+  binary = fread(fid, inf, 'uint8');
+  fclose(fid);
 
-    for j = 0:(chunks_per_file - 1)
-        start_byte = start_offset + block_spacing * j;
-        end_byte = start_byte + chunk_size - 1;
-        if end_byte <= length(binary)
-            binary_data(idx:idx+chunk_size-1) = binary(start_byte:end_byte);
-            idx = idx + chunk_size;
-        else
-            warning('Skipped incomplete chunk in %s at block %d', filename, j+1);
-        end
+  for j = 0:(chunks_per_file - 1)
+    start_byte = start_offset + block_spacing * j;
+    end_byte = start_byte + chunk_size - 1;
+    if end_byte <= length(binary)
+      binary_data(idx:idx+chunk_size-1) = binary(start_byte:end_byte);
+      idx = idx + chunk_size;
+    else
+      warning('Skipped incomplete chunk in %s at block %d', filename, j+1);
     end
+  end
 end
 
 % Trim to actual size in case some files/chunks were incomplete
@@ -57,12 +58,12 @@ num_files = length(imagefiles);
 % === Extract Image Data ===
 disp('Extracting image data from images');
 for i = 1:num_files
-    filename = ['./Images/', imagefiles(i).name];
-    [image_OK,DATA]=image_converter(filename);
-    if image_OK==1
+  filename = ['./Images/', imagefiles(i).name];
+  [image_OK,DATA]=image_converter(filename);
+  if image_OK==1
     binary_data = [binary_data;DATA'];
     images=images+1;
-    end
+  end
 end
 % Collect image files %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -73,8 +74,17 @@ fwrite(fid, binary_data, 'uint8');
 fclose(fid);
 
 % === Generate Preview Image ===
+if length(binary_data)==0;
 disp('Generating an image file for checking');
 data_viewer(binary_data, 'preview.png');
+end
+
+% === Limit maximum images to 540 ===
+if images>maximum_file_number
+  images=maximum_file_number;
+  binary_data=binary_data(1:maximum_file_number*chunk_size);
+  disp('Image are limited to 540, cutting file !');
+end
 
 % === Generate Arduino Header File ===
 disp('Generating header file for Arduino IDE');
@@ -85,22 +95,22 @@ fprintf(fid, 'const uint8_t graphical_DATA[] = {\n');
 
 % Format as hex table, 16 bytes per line
 for i = 1:length(binary_data)
-    if mod(i - 1, 16) == 0
-        fprintf(fid, '  ');
-    end
-    fprintf(fid, '0x%02X', binary_data(i));
-    if i < length(binary_data)
-        fprintf(fid, ',');
-    end
-    if mod(i, 16) == 0
-        fprintf(fid, '\n');
-    else
-        fprintf(fid, ' ');
-    end
+  if mod(i - 1, 16) == 0
+    fprintf(fid, '  ');
+  end
+  fprintf(fid, '0x%02X', binary_data(i));
+  if i < length(binary_data)
+    fprintf(fid, ',');
+  end
+  if mod(i, 16) == 0
+    fprintf(fid, '\n');
+  else
+    fprintf(fid, ' ');
+  end
 end
 
 if mod(length(binary_data), 16) ~= 0
-    fprintf(fid, '\n');
+  fprintf(fid, '\n');
 end
 
 fprintf(fid, '};\n');
